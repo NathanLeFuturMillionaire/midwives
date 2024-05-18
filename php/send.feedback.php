41d1fae7254796534e60d7376c2b7f9178230b44
Nathan Le Fütür Milliønãîre <misterntkofficiel2.0@gmail.com>
2024-05-18 23:52:43 +0100

    Enregistre les données dans la base de données

---
+++ php/send.feedback.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Content-Type: application/json');
+
+
+// On se connecte à la base de données
+try {
+  $database = new PDO("mysql:host=localhost;dbname=midwives;charset=utf8", "root", "root");
+} catch (Exception $e) {
+  // S'il y a eu une erreur, eh bah on le fait savoir
+  die('Erreur : '. $e->getMessage());
+}
+
+$firstname = strip_tags($_POST['firstname']);
+$lastname = strip_tags($_POST['lastname']);
+$email = htmlspecialchars($_POST['email']);
+$feed = htmlspecialchars($_POST['message']);
+
+// Insère les données dans la base de données
+$statement = $database->prepare("INSERT INTO feedbacks(lastname, firstname, email, feed, date_added) VALUES(?, ?, ?, ?, NOW())");
+$success = $statement->execute([$lastname, $firstname, $email, $feed]);
+
+if ($success) {
+  echo json_encode([
+    "success" => true,
+    "message" => "J'ai reçu ton message",
+  ]);
+}
